45ab076e5c78ee3a2b7af5b3ddb8a291b7223e5a
Tersornpat <sornpat@gmail.com>
2022-10-07 11:59:23 +0700

    ex7

---
+++ src/JpaDB/StudentDatabaseJPA.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JpaDB;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Tersornpat
+ */
+public class StudentDatabaseJPA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Student obj1 = new Student(1,"Tersornpat",3.14);
+        Student obj2 = new Student(2,"Jeff",2.98);
+        Student obj3 = new Student(3,"Deff",1.43);
+        Student obj4 = new Student(4,"Weff",4.00);
+        updateName(obj1, "Patty");
+        updateGpa(obj1, 4.00);
+        
+        updateNameByID(2,"Jave");
+        
+        
+        List<Student> stuList = StudentTable.findAllStudent();
+        printAllstudent(stuList);
+    }
+}
